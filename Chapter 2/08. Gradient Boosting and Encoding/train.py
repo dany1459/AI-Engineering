cfba0cdfa1858bff8c27178b29d14d4e8c44f586
Dan Adrian <dany1459@yahoo.co.uk>
2021-10-27 11:26:57 +0200

    GB & Encoding

---
+++ Chapter 2/08. Gradient Boosting and Encoding/train.py
@@ -0,0 +1,5 @@
+import data_handler as dh
+
+x_train, x_test, y_train, y_test = dh.get_data(r"C:\Users\Igor\Documents\GitHub\AI-Engineering\Chapter 2\08. Gradient Boosting and Encoding\insurance.csv")
+
+print(dh.hello)
