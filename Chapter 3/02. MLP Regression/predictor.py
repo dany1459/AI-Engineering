1229e79d0b4fed02e6d4a29da3966c8db6629229
Dan Adrian <dany1459@yahoo.co.uk>
2021-11-24 08:53:06 +0100

    Create predictor.py

---
+++ Chapter 3/02. MLP Regression/predictor.py
@@ -0,0 +1,19 @@
+import model as md
+import data_handler as dh
+import data_handler2 as dh2
+import train as tr
+import torch
+
+if __name__ == '__main__':
+    _, testloader = tr.load_data()
+    model = md.Network()
+    # model = torch.load('./02. MLP Regression/model.pth')
+    
+    model.eval()
+    with torch.no_grad():
+        for x_test, y_test in iter(testloader):
+            x_test, y_test = x_test.float(), y_test.float()
+
+            outputs_test = model(x_test)
+            print(outputs_test)
+            
