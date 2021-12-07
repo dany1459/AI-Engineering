89d2be53d47641f0df49a332548d4a35ace1ee06
Dan Adrian <dany1459@yahoo.co.uk>
2021-12-07 16:08:17 +0100

    regression

--- Chapter 3/02. MLP Regression/predictor.py
+++ Chapter 3/02. MLP Regression/predictor.py
@@ -7,13 +7,10 @@ import torch
 if __name__ == '__main__':
     _, testloader = tr.load_data()
     model = md.Network()
-    # model = torch.load('./02. MLP Regression/model.pth')
-    
+    model.load_state_dict(torch.load('./02. MLP Regression/model.pth'))
     model.eval()
     with torch.no_grad():
         for x_test, y_test in iter(testloader):
             x_test, y_test = x_test.float(), y_test.float()
 
             outputs_test = model(x_test)
-            print(outputs_test)
-            
