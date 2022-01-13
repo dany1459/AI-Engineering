2e12b24c2033f4a90f42b9f1985be202ea73ed52
Dan Adrian <dany1459@yahoo.co.uk>
2022-01-13 16:45:04 +0100

    Update app.py

--- Chapter 4/Flask/app.py
+++ Chapter 4/Flask/app.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
-@app.route("/")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
-# @app.route("/")
-# def about():
-#     return render_template("contact.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
-# @app.route("/")
-# def about():
-#     return render_template("portfolio.html")
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
 
 if __name__ == "__main__":
     app.run(debug=True) 
